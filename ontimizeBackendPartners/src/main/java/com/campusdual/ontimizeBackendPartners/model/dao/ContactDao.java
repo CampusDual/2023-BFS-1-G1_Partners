ea82bff29a12578b18d878adc1c0465bc0a54ef9
iria <105212242+IriaSanchez@users.noreply.github.com>
2023-05-26 13:45:58 +0200

    ontimize

--- ontimizeBackendPartners/src/main/java/com/campusdual/ontimizeBackendPartners/model/dao/ContactDao.java
+++
@@ -1,8 +0,0 @@
-package com.campusdual.ontimizeBackendPartners.model.dao;
-
-
-import com.campusdual.ontimizeBackendPartners.model.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactDao extends JpaRepository<Contact, Integer> {
-}
