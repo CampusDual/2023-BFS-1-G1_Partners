8f0bb1f07d615fde4feab047971dd88b461631bb
iria <105212242+IriaSanchez@users.noreply.github.com>
2023-06-05 09:58:45 +0200

    permission

--- frontEnd_ontimize/src/app/main/main-routing.module.ts
+++ frontEnd_ontimize/src/app/main/main-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuardService } from 'ontimize-web-ngx';
+import { AuthGuardService, PermissionsGuardService } from 'ontimize-web-ngx';
 
 import { MainComponent } from './main.component';
 
@@ -9,11 +9,12 @@ export const routes: Routes = [
     path: '',
     component: MainComponent,
     canActivate: [AuthGuardService],
+    canActivateChild:[PermissionsGuardService],
     children: [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+      { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule),data:{oPermission:{permissionId: 'home'}} },
       {path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) },
-      {path: 'home-partner', loadChildren: () => import('./home-partner/home-partner.module').then(m => m.HomePartnerModule) }
+      {path: 'home-partner', loadChildren: () => import('./home-partner/home-partner.module').then(m => m.HomePartnerModule),data:{oPermission:{permissionId: 'home-partner'}} }
 
 
 
