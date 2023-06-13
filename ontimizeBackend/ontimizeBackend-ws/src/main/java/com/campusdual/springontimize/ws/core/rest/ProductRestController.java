1f65d6bc3942b2d389d61979fe6be40202dc159d
iria <105212242+IriaSanchez@users.noreply.github.com>
2023-06-13 10:44:02 +0200

    producto lista

--- ontimizeBackend/ontimizeBackend-ws/src/main/java/com/campusdual/springontimize/ws/core/rest/ProductRestController.java
+++ ontimizeBackend/ontimizeBackend-ws/src/main/java/com/campusdual/springontimize/ws/core/rest/ProductRestController.java
@@ -2,17 +2,10 @@ package com.campusdual.springontimize.ws.core.rest;
 
 
 import com.campusdual.springontimize.api.core.service.IProductService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.campusdual.springontimize.api.core.service.IUserService;
-import com.ontimize.jee.common.dto.EntityResult;
 import com.ontimize.jee.server.rest.ORestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
@@ -27,12 +20,5 @@ public class ProductRestController extends ORestController<IProductService> {
         return this.productService;
     }
 
-    @RequestMapping(
-            value = "/product",
-            method = RequestMethod.POST,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<EntityResult> login() {
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
 
 }
