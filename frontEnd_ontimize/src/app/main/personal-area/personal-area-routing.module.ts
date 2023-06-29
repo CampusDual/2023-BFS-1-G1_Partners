d1d298e938301516f50257006ed3c3e38b7c75f6
iria <105212242+IriaSanchez@users.noreply.github.com>
2023-06-29 14:27:32 +0200

    Imagen documentos

--- frontEnd_ontimize/src/app/main/personal-area/personal-area-routing.module.ts
+++ frontEnd_ontimize/src/app/main/personal-area/personal-area-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PersonalAreaComponent } from './personal-area-home/personal-area.component';
+import { PersonalAreaDetailComponent } from './personal-area-detail/personal-area-detail.component';
 
 const routes: Routes = [{
 
@@ -8,10 +9,11 @@ const routes: Routes = [{
   path: "",
   component: PersonalAreaComponent
 },
-
-
-
-
+{
+  path: 'personal-area-detail/:id',
+    component: PersonalAreaDetailComponent
+  }
+  
 ];
 
 @NgModule({
