3b2f5dc30883b6ee74ca56150e6d6d8e5b87e1c7
abrilyanhez <abrildarriba@gmail.com>
2023-06-29 14:27:54 +0200

    personal detail

--- frontEnd_ontimize/src/app/main/personal-area/personal-area-routing.module.ts
+++ frontEnd_ontimize/src/app/main/personal-area/personal-area-routing.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PersonalAreaComponent } from './personal-area-home/personal-area.component';
-
+import { PersonalAreaDetailComponent } from './personal-area-detail/personal-area-detail.component';
 const routes: Routes = [{
-
 
   path: "",
   component: PersonalAreaComponent
 },
 
-
-
+{
+path: 'personal-area-detail/:id',
+  component: PersonalAreaDetailComponent
+}
 
 ];
 
