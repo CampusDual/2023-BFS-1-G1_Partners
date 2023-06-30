5807a7b39928706b3f7bab9c0b3be071f0b0386a
iria <105212242+IriaSanchez@users.noreply.github.com>
2023-06-30 10:00:56 +0200

    COMO partner QUIERO poder descargar los documentos que desee de mis productos

--- frontEnd_ontimize/src/app/main/product/product-home/form-product-detail/form-product-detail.component.ts
+++ frontEnd_ontimize/src/app/main/product/product-home/form-product-detail/form-product-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Injector, OnInit, ViewChild } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material';
+import { DialogService, OFileInputComponent, OFormComponent, OTableComponent, OntimizeService } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'app-form-product-detail',
@@ -7,11 +10,76 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class FormProductDetailComponent implements OnInit {
 
-  @Input() selectedProduct: any;
+  comboVisible = false;
+  protected productService: OntimizeService;
 
-  constructor() { }
+   productForm: FormGroup;
+   @ViewChild('formproducts', { static: false }) formProducts: OFormComponent;
+   @ViewChild('partnersTable', {static: false }) public partnersTable: OTableComponent;
+   @ViewChild('fileinput',{static:true}) fileInput:OFileInputComponent;
+   @ViewChild('filetable',{static:true}) fileTable:OTableComponent;
 
+   constructor(private formBuilder: FormBuilder, protected dialog: MatDialog,protected dialogService: DialogService,public injector: Injector) {
+    this.productService = this.injector.get(OntimizeService);
+   }
+   
   ngOnInit() {
+    const conf = this.productService.getDefaultServiceConfiguration('products');
+    this.productService.configureService(conf);
   }
 
+    getFileData(){
+      if(this.formProducts){
+      return {product_id:this.formProducts.getDataValue('id')};
+    }else{
+      return {};
+    }
+    }
+  
+    
+  
+    actionClick(event){
+      this.productService.query({id:event.id}, ['name','base64'], 'fileContent').subscribe(res => {
+        if (res.data && res.data.length) {
+          let filename = res.data[0].name;
+          let base64 = res.data[0].base64;
+          const src = `data:text/csv;base64,${base64}`;
+          const link = document.createElement("a");
+          link.href = src;
+          link.download = filename;
+          link.click();
+          link.remove();
+        }
+      });
+      
+    }
+  
+    onError(event){
+      if(event.status === 507){
+        this.showError(event);
+      }
+      this.fileInput.clearValue();
+    }
+  
+    showError(event: any) {
+      if (this.dialogService) {
+      this.dialogService.error('Error',
+          'El fichero ya existe');
+      }
+    }
+
+
+
+
+
+  onSave() {
+    if (this.productForm.valid) {
+      console.log('Datos válidos, guardando en la base de datos...');
+    } else {
+      console.log('Datos inválidos, no se puede guardar en la base de datos.');
+    }
+  }
 }
+
+
+
