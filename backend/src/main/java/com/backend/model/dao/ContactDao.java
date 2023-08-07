1c19608871ab269185ce77dba4a0cde2aa36a97f
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2023-08-07 13:59:07 +0200

    Add Dockerfiles for DB, backend and frontend. Adapt SLQ dump to restore DB. Adapt backend and frontend apps to run from docker

--- backend/src/main/java/com/backend/model/dao/ContactDao.java
+++
@@ -1,8 +0,0 @@
-package com.backend.model.dao;
-
-
-import com.backend.model.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactDao extends JpaRepository<Contact, Integer> {
-}
